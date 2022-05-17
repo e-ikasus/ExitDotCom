fbc6249363bca710b3e5cd5afb3be00ded705661
ACpourENI <annechloe.rioux2022@campus-eni.fr>
2022-05-17 18:39:18 +0200

    Formulaire Sortie + légères modifs list Sortie

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,28 +2,64 @@
 
 namespace App\Form;
 
+use App\Entity\Campus;
+use App\Entity\Lieu;
 use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
-            ->add('nbInscriptionsMax')
-            ->add('infosSortie')
-            ->add('Lieu')
-            ->add('etat')
-            ->add('campus')
-            ->add('organisateur')
-            ->add('participants')
-        ;
+            ->add('nom', null, [
+                'label' => 'Nom de la sortie : '
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label' => 'Date et heure de la sortie : ',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label' => 'Date limite d\'inscription : ',
+                'html5' => true,
+                'widget' => 'single_text'
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombre de places : ',
+                'attr' => ['min' => 1]
+            ])
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée : ',
+                'attr' => ['min' => 30, 'step' => 15],
+//                'constraints' => [
+//                    new NotBlank([
+//                        'message' => 'Veuillez rentrer une durée',
+//                    ]),
+//                    new Length([
+//                        'min' => 30,
+//                        'minMessage' => 'Your message should be at least {{ limit }} characters',
+//                        // max length allowed by Symfony for security reasons
+//                        'max' => 4096,
+//        ]),
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label' => 'Description et infos : ',
+                'attr' => ['rows' => 5, 'cols' => 50],
+            ])
+            ->add('Lieu', EntityType::class, [
+                'label' => 'Lieu : ',
+                'class' => Lieu::class,
+                'choice_label' => 'nom'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
