ad29cf16365c6bdfb31e203652c80950aa960050
Deadrix <Alexandre66480@hotmail.fr>
2022-05-17 02:38:26 +0200

    Passage du User admin en premier user. Hachage en BCRYPT des mots de passe générés sinon pas de connexion possible. Révision des imports.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,176 +2,179 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
 use App\Entity\Etat;
+use App\Entity\Lieu;
 use App\Entity\Participant;
+use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker;
 
 class AppFixtures extends Fixture
 {
-	const NBR_CAMPUS = 10;
-	const NBR_VILLES = 30;
-	const NBR_LIEUX = 10;
-	const NBR_PARTICIPANTS = 50;
-	const NBR_SORTIES = 70;
-	const NBR_ETATS = 6;
+    const NBR_CAMPUS = 10;
+    const NBR_VILLES = 30;
+    const NBR_LIEUX = 10;
+    const NBR_PARTICIPANTS = 50;
+    const NBR_SORTIES = 70;
+    const NBR_ETATS = 6;
 
-	const MAX_PARTICIPANTS_PAR_SORTIE = 10;
+    const MAX_PARTICIPANTS_PAR_SORTIE = 10;
 
-	public function load(ObjectManager $manager): void
-	{
-		$faker = Faker\Factory::create('fr_FR');
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
 
-		/********************/
-		/* Liste des villes */
-		/********************/
+        /********************/
+        /* Liste des villes */
+        /********************/
 
-		$villes = array();
+        $villes = array();
 
-		for ($i = 0; $i < self::NBR_VILLES; $i++)
-		{
-			$villes[$i] = new \App\Entity\Ville();
-			$villes[$i]->setNom($faker->city());
-			$villes[$i]->setCodePostal($faker->postcode());
+        for ($i = 0; $i < self::NBR_VILLES; $i++) {
+            $villes[$i] = new Ville();
+            $villes[$i]->setNom($faker->city());
+            $villes[$i]->setCodePostal($faker->postcode());
 
-			$manager->persist($villes[$i]);
-		}
+            $manager->persist($villes[$i]);
+        }
 
-		$manager->flush();
+        $manager->flush();
 
-		/*******************/
-		/* Liste des lieux */
-		/*******************/
+        /*******************/
+        /* Liste des lieux */
+        /*******************/
 
-		$lieux = array();
+        $lieux = array();
 
-		for ($i = 0; $i < self::NBR_LIEUX; $i++)
-		{
-			$lieux[$i] = new \App\Entity\Lieu();
-			$lieux[$i]->setNom($faker->userName());
-			$lieux[$i]->setRue($faker->streetAddress());
-			$lieux[$i]->setLatitude($faker->latitude());
-			$lieux[$i]->setLongitude($faker->longitude());
+        for ($i = 0; $i < self::NBR_LIEUX; $i++) {
+            $lieux[$i] = new Lieu();
+            $lieux[$i]->setNom($faker->userName());
+            $lieux[$i]->setRue($faker->streetAddress());
+            $lieux[$i]->setLatitude($faker->latitude());
+            $lieux[$i]->setLongitude($faker->longitude());
 
-			$villes[rand(0, self::NBR_VILLES - 1)]->addLieux($lieux[$i]);
+            $villes[rand(0, self::NBR_VILLES - 1)]->addLieux($lieux[$i]);
 
-			$manager->persist($lieux[$i]);
-		}
+            $manager->persist($lieux[$i]);
+        }
 
-		$manager->flush();
+        $manager->flush();
 
-		/********************/
-		/* Liste des campus */
-		/********************/
+        /********************/
+        /* Liste des campus */
+        /********************/
 
-		$campus = array();
+        $campus = array();
 
-		for ($i = 0; $i < self::NBR_CAMPUS; $i++)
-		{
-			$campus[$i] = new \App\Entity\Campus();
-			$campus[$i]->setNom($faker->company());
+        for ($i = 0; $i < self::NBR_CAMPUS; $i++) {
+            $campus[$i] = new Campus();
+            $campus[$i]->setNom($faker->company());
 
-			$manager->persist($campus[$i]);
-		}
+            $manager->persist($campus[$i]);
+        }
 
-		$manager->flush();
+        $manager->flush();
 
-		/*******************/
-		/* Liste des états */
-		/*******************/
+        /*******************/
+        /* Liste des états */
+        /*******************/
 
-		$etats = array();
+        $etats = array();
 
-		$etats[0] = (new Etat())->setLibelle("créée");
-		$etats[1] = (new Etat())->setLibelle("ouverte");
-		$etats[2] = (new Etat())->setLibelle("clôturée");
-		$etats[3] = (new Etat())->setLibelle("activité en cours");
-		$etats[4] = (new Etat())->setLibelle("passée");
-		$etats[5] = (new Etat())->setLibelle("annulée");
+        $etats[0] = (new Etat())->setLibelle("créée");
+        $etats[1] = (new Etat())->setLibelle("ouverte");
+        $etats[2] = (new Etat())->setLibelle("clôturée");
+        $etats[3] = (new Etat())->setLibelle("activité en cours");
+        $etats[4] = (new Etat())->setLibelle("passée");
+        $etats[5] = (new Etat())->setLibelle("annulée");
 
-		for ($i = 0; $i < 6; $i++) $manager->persist($etats[$i]);
+        for ($i = 0; $i < 6; $i++) $manager->persist($etats[$i]);
 
-		$manager->flush();
-
-		/**************************/
-		/* Liste des participants */
-		/**************************/
-
-		$participants = array();
-
-		for ($i = 0; $i < self::NBR_PARTICIPANTS; $i++)
-		{
-			$participants[$i] = new \App\Entity\Participant();
-			$participants[$i]->setNom($faker->lastName());
-			$participants[$i]->setPrenom($faker->firstName());
-			$participants[$i]->setPseudo($faker->userName());
-			$participants[$i]->setPassword($faker->password());
-			$participants[$i]->setEmail($faker->email());
-			$participants[$i]->setTelephone($faker->phoneNumber());
-			$participants[$i]->setAdministrateur(false);
-			$participants[$i]->setActif(true);
-
-			$campus[rand(0, self::NBR_CAMPUS - 1)]->addParticipant($participants[$i]);
-
-			$manager->persist($participants[$i]);
-		}
-
-		$manager->flush();
+        $manager->flush();
 
         /********************/
         /* Ajouter un Admin */
         /********************/
 
         $adminUser = new Participant();
+        $adminUser->setRoles(["ROLE_ADMIN"]);
         $adminUser->setNom("admin");
         $adminUser->setPrenom("admin");
         $adminUser->setPseudo("admin");
-        $adminUser->setPassword("admin");
+        $adminUser->setPassword(password_hash('admin', PASSWORD_BCRYPT));
         $adminUser->setEmail("admin@admin.fr");
-        $adminUser->setTelephone(0601020304);
+        $adminUser->setTelephone("0601020304");
         $adminUser->setAdministrateur(true);
         $adminUser->setActif(true);
+
+        $campus[rand(0, self::NBR_CAMPUS - 1)]->addParticipant($adminUser);
 
         $manager->persist($adminUser);
         $manager->flush();
 
-		/*********************/
-		/* Liste des sorties */
-		/*********************/
+        /**************************/
+        /* Liste des participants */
+        /**************************/
 
-		$sorties = array();
+        $participants = array();
 
-		for ($i = 0; $i < self::NBR_SORTIES; $i++)
-		{
-			$sorties[$i] = new \App\Entity\Sortie();
-			$sorties[$i]->setnom($faker->colorName());
-			$sorties[$i]->setDateHeureDebut($faker->dateTime());
-			$sorties[$i]->setDuree(rand(1, 23));
-			$sorties[$i]->setDateLimiteInscription($faker->dateTime());
-			$sorties[$i]->setNbInscriptionsMax(rand(1, 20));
-			$sorties[$i]->setInfosSortie($faker->text());
+        for ($i = 0; $i < self::NBR_PARTICIPANTS; $i++) {
+            $participants[$i] = new Participant();
 
-			$campus[rand(0, self::NBR_CAMPUS - 1)]->addSortieOrganisee($sorties[$i]);
+            $participants[$i]->setRoles(["ROLE_USER"]);
+            $participants[$i]->setNom($faker->lastName());
+            $participants[$i]->setPrenom($faker->firstName());
+            $participants[$i]->setPseudo($faker->userName());
+            $participants[$i]->setPassword(password_hash($faker->password(), PASSWORD_BCRYPT));
+            $participants[$i]->setEmail($faker->email());
+            $participants[$i]->setTelephone($faker->phoneNumber());
+            $participants[$i]->setAdministrateur(false);
+            $participants[$i]->setActif(true);
 
-			$lieux[rand(0, self::NBR_LIEUX - 1)]->addSorty($sorties[$i]);
+            $campus[rand(0, self::NBR_CAMPUS - 1)]->addParticipant($participants[$i]);
 
-			$etats[rand(0, self::NBR_ETATS - 1)]->addSorty($sorties[$i]);
+            $manager->persist($participants[$i]);
+        }
 
-			$participants[rand(0, self::NBR_PARTICIPANTS - 1)]->addSortiesOrganisee($sorties[$i]);
+        $manager->flush();
 
-			for ($j = 0; $j < rand(0, self::MAX_PARTICIPANTS_PAR_SORTIE); $j++)
-			{
-				$part = $participants[rand(0, self::NBR_PARTICIPANTS - 1)];
+        /*********************/
+        /* Liste des sorties */
+        /*********************/
 
-				while ($sorties[$i]->getParticipants()->contains($part)) $part = $participants[rand(0, self::NBR_PARTICIPANTS - 1)];
+        $sorties = array();
 
-				$sorties[$i]->addParticipant($part);
-			}
+        for ($i = 0; $i < self::NBR_SORTIES; $i++) {
+            $sorties[$i] = new Sortie();
+            $sorties[$i]->setnom($faker->colorName());
+            $sorties[$i]->setDateHeureDebut($faker->dateTime());
+            $sorties[$i]->setDuree(rand(1, 23));
+            $sorties[$i]->setDateLimiteInscription($faker->dateTime());
+            $sorties[$i]->setNbInscriptionsMax(rand(1, 20));
+            $sorties[$i]->setInfosSortie($faker->text());
 
-			$manager->persist($sorties[$i]);
-		}
+            $campus[rand(0, self::NBR_CAMPUS - 1)]->addSortieOrganisee($sorties[$i]);
 
-		$manager->flush();
-	}
+            $lieux[rand(0, self::NBR_LIEUX - 1)]->addSorty($sorties[$i]);
+
+            $etats[rand(0, self::NBR_ETATS - 1)]->addSorty($sorties[$i]);
+
+            $participants[rand(0, self::NBR_PARTICIPANTS - 1)]->addSortiesOrganisee($sorties[$i]);
+
+            for ($j = 0; $j < rand(0, self::MAX_PARTICIPANTS_PAR_SORTIE); $j++) {
+                $part = $participants[rand(0, self::NBR_PARTICIPANTS - 1)];
+
+                while ($sorties[$i]->getParticipants()->contains($part)) $part = $participants[rand(0, self::NBR_PARTICIPANTS - 1)];
+
+                $sorties[$i]->addParticipant($part);
+            }
+
+            $manager->persist($sorties[$i]);
+        }
+
+        $manager->flush();
+    }
 }
