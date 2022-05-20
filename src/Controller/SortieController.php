3a96ed597cfb230abb3b653c3096789acfc28e6c
ACpourENI <annechloe.rioux2022@campus-eni.fr>
2022-05-20 17:25:41 +0200

    Affichage des participants à une sortie sur son détail.

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,6 +7,7 @@ use App\Entity\Sortie;
 use App\Form\RechercheSortiesType;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use App\Services\Research;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,42 +21,39 @@ use Symfony\Component\Security\Core\Security;
  */
 class SortieController extends AbstractController
 {
-	public function __construct()
-	{
-		$this->research = new Research();
-	}
+    public function __construct()
+    {
+        $this->research = new Research();
+    }
 
-	/**
-	 * @Route("/list", name="sortie_list", methods={"POST", "GET"})
-	 */
-	public function searchInList(Security $security, SortieRepository $sortieRepository, Request $request): Response
-	{
-		$user = $security->getUser();
+    /**
+     * @Route("/list", name="sortie_list", methods={"POST", "GET"})
+     */
+    public function searchInList(Security $security, SortieRepository $sortieRepository, Request $request): Response
+    {
+        $user = $security->getUser();
 
-		$form = $this->createForm(RechercheSortiesType::class, $this->research);
-		$form->handleRequest($request);
+        $form = $this->createForm(RechercheSortiesType::class, $this->research);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid())
-		{
-			$sorties = $sortieRepository->findByCreteria($user, $this->research);
-		}
-		else
-		{
-			$sorties = $sortieRepository->findAll();
-		}
-		return $this->render('sortie/list.html.twig', [
-						'sorties' => $sorties,
-						'form' => $form->createView()
-				]
-		);
-	}
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sorties = $sortieRepository->findByCreteria($user, $this->research);
+        } else {
+            $sorties = $sortieRepository->findAll();
+        }
+        return $this->render('sortie/list.html.twig', [
+                'sorties' => $sorties,
+                'form' => $form->createView()
+            ]
+        );
+    }
 
-	/**
-	 * @Route("/new", name="sortie_new", methods={"GET", "POST"})
-	 */
-	public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, Security $security): Response
-	{
-		$sortie = new Sortie();
+    /**
+     * @Route("/new", name="sortie_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, Security $security): Response
+    {
+        $sortie = new Sortie();
 
         //Nous allons chercher les différents états via l'EtatRepository.
         //Nous créons une méthode qui sera auto-interprétée par Symfony pour aller chercher le bon libellé selon la constante dans l'entity Etat.
@@ -69,63 +67,60 @@ class SortieController extends AbstractController
 
         $sortie->setOrganisateur($user);
 
-		$form = $this->createForm(SortieType::class, $sortie);
-		$form->handleRequest($request);
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid())
-		{
-			$sortieRepository->add($sortie, true);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie, true);
 
-			return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
-		}
+            return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
+        }
 
-		return $this->renderForm('sortie/new.html.twig', [
-				'sortie' => $sortie,
-				'form' => $form,
-		]);
-	}
+        return $this->renderForm('sortie/new.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}", name="sortie_show", methods={"GET"})
-	 */
-	public function show(Sortie $sortie): Response
-	{
+    /**
+     * @Route("/{id}", name="sortie_show", methods={"GET"})
+     */
+    public function show(Sortie $sortie): Response
+    {
 		return $this->render('sortie/show.html.twig', [
-				'sortie' => $sortie,
-		]);
+            'sortie' => $sortie,
+        ]);
 	}
 
-	/**
-	 * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
-	 */
-	public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
-	{
-		$form = $this->createForm(SortieType::class, $sortie);
-		$form->handleRequest($request);
+    /**
+     * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        $form = $this->createForm(SortieType::class, $sortie);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid())
-		{
-			$sortieRepository->add($sortie, true);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie, true);
 
-			return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
-		}
+            return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
+        }
 
-		return $this->renderForm('sortie/edit.html.twig', [
-				'sortie' => $sortie,
-				'form' => $form,
-		]);
-	}
+        return $this->renderForm('sortie/edit.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
 
-	/**
-	 * @Route("/{id}", name="sortie_delete", methods={"POST"})
-	 */
-	public function delete( Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
-	{
-		if ($this->isCsrfTokenValid('delete' . $sortie->getId(), $request->request->get('_token')))
-		{
-			$sortieRepository->remove($sortie, true);
-		}
+    /**
+     * @Route("/{id}", name="sortie_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $sortie->getId(), $request->request->get('_token'))) {
+            $sortieRepository->remove($sortie, true);
+        }
 
-		return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
-	}
+        return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
+    }
 }
