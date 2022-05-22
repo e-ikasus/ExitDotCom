3d7652b92d7e193ae87df6e4f7937e1f9495fa11
Deadrix <Alexandre66480@hotmail.fr>
2022-05-22 23:56:36 +0200

    Réunir les formulaires new et edit des sorties et les methode correspondante dans le contrôleur

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
-use App\Entity\Etat;
+use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\RechercheSortiesType;
 use App\Form\SortieType;
 use App\Repository\EtatRepository;
-use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use App\Services\Research;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -50,22 +49,33 @@ class SortieController extends AbstractController
 
     /**
      * @Route("/new", name="sortie_new", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
      */
     public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, Security $security): Response
     {
-        $sortie = new Sortie();
+        if ($request->get('_route') == "sortie_new") {
 
-        //Nous allons chercher les différents états via l'EtatRepository.
-        //Nous créons une méthode qui sera auto-interprétée par Symfony pour aller chercher le bon libellé selon la constante dans l'entity Etat.
-        $etatCreee = $etatRepository->findByIdLibelle(Etat::CREEE);
-        $sortie->setEtat($etatCreee[0]);
+            $sortie = new Sortie();
 
-        //Nous récupérons l'instance du participant connecté.
-        //Puis nous avons fixé la propriété campus de sortie selon le participant.
-        $user = $security->getUser();
-        $sortie->setCampus($user->getCampus());
+            //Nous allons chercher les différents états via l'EtatRepository.
+            //Nous créons une méthode qui sera auto-interprétée par Symfony pour aller chercher le bon libellé selon la constante dans l'entity Etat.
 
-        $sortie->setOrganisateur($user);
+            // $etatCreee = $etatRepository->findByIdLibelle(Etat::CREEE);
+            // $sortie->setEtat($etatCreee[0]);
+            //Methode plus adaptée pour faire la meme requête il me semble...
+            $etatCreee = $etatRepository->findOneBy(array('libelle' => 'créée'));
+            $sortie->setEtat($etatCreee);
+
+            //Nous récupérons l'instance du participant connecté.
+            //Puis nous avons fixé la propriété campus de sortie selon le participant.
+            $user = $security->getUser();
+            $sortie->setCampus($user->getCampus());
+
+            $sortie->setOrganisateur($user);
+        } else {
+
+            $sortie = $sortieRepository->findOneBy(array('id' => $request->get('id')));
+        }
 
         $form = $this->createForm(SortieType::class, $sortie);
         $form->handleRequest($request);
@@ -87,30 +97,30 @@ class SortieController extends AbstractController
      */
     public function show(Sortie $sortie): Response
     {
-		return $this->render('sortie/show.html.twig', [
+        return $this->render('sortie/show.html.twig', [
             'sortie' => $sortie,
-        ]);
-	}
-
-    /**
-     * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
-    {
-        $form = $this->createForm(SortieType::class, $sortie);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $sortieRepository->add($sortie, true);
-
-            return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('sortie/edit.html.twig', [
-            'sortie' => $sortie,
-            'form' => $form,
         ]);
     }
+
+//    /**
+//     * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
+//     */
+//    public function edit(Request $request, Sortie $sortie, SortieRepository $sortieRepository): Response
+//    {
+//        $form = $this->createForm(SortieType::class, $sortie);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $sortieRepository->add($sortie, true);
+//
+//            return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
+//        }
+//
+//        return $this->renderForm('sortie/edit.html.twig', [
+//            'sortie' => $sortie,
+//            'form' => $form,
+//        ]);
+//    }
 
     /**
      * @Route("/{id}", name="sortie_delete", methods={"POST"})
