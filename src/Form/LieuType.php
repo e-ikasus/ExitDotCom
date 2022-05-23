52f950612a7ac8ebd5ac80db37628f210f990563
zouaouikais <zouaoui.kais.pro@gmail.com>
2022-05-23 13:33:44 +0200

    mise en page ajout lieu

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -6,6 +6,7 @@ use App\Entity\Lieu;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,16 +15,38 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('rue')
-            ->add('latitude')
-            ->add('longitude')
-            ->add('ville', EntityType::class, [
-                'label' => 'Ville',
-                'class' => Ville::class,
-                'choice_label' => 'nom'
+            ->add('nom', TextType::class, [
+                'label' => 'Nom : ',
+                'attr' => [
+                    'class' => 'm-2 px-2'
+                ],
             ])
-        ;
+            ->add('rue', TextType::class, [
+                'label' => 'Rue : ',
+                'attr' => [
+                    'class' => 'm-2 px-2'
+                ],
+            ])
+            ->add('latitude', TextType::class, [
+                'label' => 'Latitude : ',
+                'attr' => [
+                    'class' => 'm-2 px-2'
+                ],
+            ])
+            ->add('longitude', TextType::class, [
+                'label' => 'Longitude : ',
+                'attr' => [
+                    'class' => 'm-2 px-2'
+                ],
+            ])
+            ->add('ville', EntityType::class, [
+                'label' => 'Ville : ',
+                'class' => Ville::class,
+                'choice_label' => 'nom',
+                'attr' => [
+                    'class' => 'm-2 px-2'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
