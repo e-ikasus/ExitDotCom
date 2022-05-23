457a665cb9ce4df64a3a72819d8e18f57f9b6ef4
ACpourENI <annechloe.rioux2022@campus-eni.fr>
2022-05-23 21:13:09 +0200

    Changement état suivant le clique sur le bouton publier ou enregistrer.

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -80,54 +80,66 @@ class SortieController extends AbstractController
 				return $this->redirectToRoute("sortie_list");
 		}
 
-		/**
-		 * @Route("/new", name="sortie_new", methods={"GET", "POST"})
-		 * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
-		 */
-		public
-		function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, Security $security): Response
-		{
-				if ($request->get('_route') == "sortie_new")
-				{
+    /**
+     * @Route("/new", name="sortie_new", methods={"GET", "POST"})
+     * @Route("/{id}/edit", name="sortie_edit", methods={"GET", "POST"})
+     */
+    public function new(Request $request, SortieRepository $sortieRepository, EtatRepository $etatRepository, Security $security): Response
+    {
+        if ($request->get('_route') == "sortie_new") {
 
-						$sortie = new Sortie();
+            $sortie = new Sortie();
 
-						//Nous allons chercher les différents états via l'EtatRepository.
-						//Nous créons une méthode qui sera auto-interprétée par Symfony pour aller chercher le bon libellé selon la constante dans l'entity Etat.
+            $form = $this->createForm(SortieType::class, $sortie);
+            $form->handleRequest($request);
 
-						// $etatCreee = $etatRepository->findByIdLibelle(Etat::OUVERTE);
-						// $sortie->setEtat($etatCreee[0]);
-						//Methode plus adaptée pour faire la meme requête il me semble...
-						$etatCreee = $etatRepository->findOneBy(array('idLibelle' => Etat::OUVERTE));
-						$sortie->setEtat($etatCreee);
+            //Nous allons chercher les différents états via l'EtatRepository.
+            //Nous créons une méthode qui sera auto-interprétée par Symfony pour aller chercher le bon libellé selon la constante dans l'entity Etat.
 
-						//Nous récupérons l'instance du participant connecté.
-						//Puis nous avons fixé la propriété campus de sortie selon le participant.
-						$user = $security->getUser();
-						$sortie->setCampus($user->getCampus());
+            // $etatCreee = $etatRepository->findByIdLibelle(Etat::OUVERTE);
+            // $sortie->setEtat($etatCreee[0]);
+            //Methode plus adaptée pour faire la meme requête il me semble...
 
-						$sortie->setOrganisateur($user);
-				} else
-				{
+            if ($form->get('enregistrer')) {
+                $etatCreee = $etatRepository->findOneBy(array('idLibelle' => Etat::ENCREATION));
+            } else {
+                $etatCreee = $etatRepository->findOneBy(array('idLibelle' => Etat::OUVERTE));
+            }
+            $sortie->setEtat($etatCreee);
 
-						$sortie = $sortieRepository->findOneBy(array('id' => $request->get('id')));
-				}
+            //Nous récupérons l'instance du participant connecté.
+            //Puis nous avons fixé la propriété campus de sortie selon le participant.
+            $user = $security->getUser();
+            $sortie->setCampus($user->getCampus());
 
-				$form = $this->createForm(SortieType::class, $sortie);
-				$form->handleRequest($request);
+            $sortie->setOrganisateur($user);
 
-				if ($form->isSubmitted() && $form->isValid())
-				{
-						$sortieRepository->add($sortie, true);
+        } else {
 
-						return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
-				}
+            $sortie = $sortieRepository->findOneBy(array('id' => $request->get('id')));
+            $form = $this->createForm(SortieType::class, $sortie);
+            $form->handleRequest($request);
 
-				return $this->renderForm('sortie/new.html.twig', [
-						'sortie' => $sortie,
-						'form' => $form,
-				]);
-		}
+            if ($form->get('enregistrer')) {
+                $etatCreee = $etatRepository->findOneBy(array('idLibelle' => Etat::ENCREATION));
+            } else {
+                $etatCreee = $etatRepository->findOneBy(array('idLibelle' => Etat::OUVERTE));
+            }
+            $sortie->setEtat($etatCreee);
+        }
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sortieRepository->add($sortie, true);
+
+            return $this->redirectToRoute('sortie_list', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('sortie/new.html.twig', [
+            'sortie' => $sortie,
+            'form' => $form,
+        ]);
+    }
 
 		/**
 		 * @Route("/{id}", name="sortie_show", methods={"GET"})
@@ -172,7 +184,7 @@ class SortieController extends AbstractController
 //    }
 
 		/**
-		 * @Route("/{id}/test", name="sortie_cancellation", methods={"GET", "POST"})
+		 * @Route("/{id}/cancel", name="sortie_cancellation", methods={"GET", "POST"})
 		 */
 		public function cancel(Request $request, Sortie $sortie, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
 		{
